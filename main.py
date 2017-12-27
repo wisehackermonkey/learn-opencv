630af3c45ed343166642da10f7209a1486abfd99
Oran C <oranbusiness@gmail.com>
2017-12-26 18:09:58 -0800

    Created Old Tv Effect
    mistanly made old tv effect

--- main.py
+++ main.py
@@ -1,31 +1,28 @@
 import cv2
 import numpy as np
 
-
-# Load an color image in grayscale
-img = cv2.imread('image.jpg',0)
-cv2.imshow('image',img)
-
-
-
 cap = cv2.VideoCapture(0)
 
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
 
-    # Our operations on the frame come here
-    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # blur = cv2.GaussianBlur(frame, (55,55), 0)
+    height, width, channels = frame.shape
 
-    # Display the resulting frame
-    cv2.imshow('Blur',blur)
+    b, g, red_colors = cv2.split(frame)
+
+    empty_image = np.empty([height, width, 1],  'uint8')
+
+
+    cv2.imshow("Red Colors", empty_image)
+
+
 
     # Dilate video
     kernal = np.ones((5,5), 'uint8')
     dilate = cv2.dilate(frame, kernal, iterations=1)
 
-    cv2.imshow('Diated', dilate)
+    # cv2.imshow('Diated', dilate)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
