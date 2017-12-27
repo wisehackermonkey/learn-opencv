98ff01c55871adc036e4ddd7a9e31d4296894409
Oran C <oranbusiness@gmail.com>
2017-12-26 23:02:40 -0800

    trying out draw circle

--- main.py
+++ main.py
@@ -6,29 +6,30 @@ import numpy as np
 img = cv2.imread('image.jpg',0)
 cv2.imshow('image',img)
 
+def draw_circle(event,x,y,flags,param):
+    global mouseX,mouseY
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        cv2.circle(img,(x,y),100,(255,0,0),-1)
+        mouseX,mouseY = x,y
 
 
 cap = cv2.VideoCapture(0)
 
 while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+	# Capture frame-by-frame
+	ret, frame = cap.read()
+	cv2.imshow('Camera', frame)
+ 
+	# Rotation
+	# M - capital letters are convention for matrix operations
+	#(0,0) - origin of rotation, -30 - minus 30 degrees of rotation, 1 - not sure
+	# M = cv2.getRotationMatrix2D((0,0), -30, 1)
+	# rotated = cv2.warpAffine(img, M, (img.shape[1], img.shape[0]))
+	# cv2.imshow("Rotated", rotated)
 
-    # Our operations on the frame come here
-    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # blur = cv2.GaussianBlur(frame, (55,55), 0)
 
-    # Display the resulting frame
-    cv2.imshow('Blur',blur)
-
-    # Dilate video
-    kernal = np.ones((5,5), 'uint8')
-    dilate = cv2.dilate(frame, kernal, iterations=1)
-
-    cv2.imshow('Diated', dilate)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+	    break
 
 # When everything done, release the capture
 cap.release()
